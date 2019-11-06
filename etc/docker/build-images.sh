27ad5ca5e23845b924c0784357fe25632c402ddf
Roberto Sánchez <rsanchez@qualityobjects.com>
2019-11-06 13:48:51 +0100

    Added Docker files

---
+++ etc/docker/build-images.sh
@@ -0,0 +1,33 @@
+export BUILD_FRONT=true
+export BUILD_BACK=true
+export TARGET=develop
+export WORKSPACE=$(pwd)
+
+if [ "$BUILD_FRONT" = "true" ]; then
+  echo Construimos front...
+  git clone https://gitlab.com/qo-oss/h3lp3r/h3lp3r-front.git
+  cd h3lp3r-front
+  git checkout ${TARGET} 
+  cd ..
+  tar -cvz -f h3lp3r-front-sources.tgz --exclude="node_modules" --exclude="dist" --exclude=".git" h3lp3r-front/ 
+  cd h3lp3r-front/etc/docker/
+  mv "$WORKSPACE/h3lp3r-front-sources.tgz" .
+  docker build -t h3lp3r-front -t qualityobjects/h3lp3r-front --rm -f ./Dockerfile -m 8GB .
+fi
+
+
+if [ "$BUILD_BACK" = "true" ]; then
+  echo Construimos back...
+  cd "$WORKSPACE"
+  git clone https://gitlab.com/qo-oss/h3lp3r/h3lp3r-back.git
+  
+  cd h3lp3r-back
+  git checkout ${TARGET} 
+  cd ..
+  tar -cvz -f h3lp3r-back-sources.tgz --exclude="*.jar" --exclude="*.tgz" --exclude="target" --exclude=".git" h3lp3r-back/ 
+  cd h3lp3r-back/etc/docker
+  mv "$WORKSPACE/h3lp3r-back-sources.tgz" .
+  docker build -t h3lp3r-back -t qualityobjects/h3lp3r-back --rm -f ./Dockerfile -m 4GB .
+fi
+
+
---
+++ etc/docker/upload-images.sh
@@ -0,0 +1,6 @@
+docker login -u qualityobjects
+
+docker push qualityobjects/h3lp3r-front
+docker push qualityobjects/h3lp3r-back 
+
+
