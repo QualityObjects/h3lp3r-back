1327f11346ba2ce75d16139d60d4ed466c679001
Roberto Sánchez <rsanchez@qualityobjects.com>
2019-11-07 15:38:33 +0100

    Added example to build an image with  a web page

---
+++ etc/meetup/web-image/build-image.sh
@@ -0,0 +1,15 @@
+docker build -t h3lp3r-web-tmp -t qualityobjects/h3lp3r-web-tmp --rm -f ./Dockerfile .
+docker push qualityobjects/h3lp3r-web-tmp:latest
+
+
+#For testing
+# docker run -it --rm -p 1080:80 h3lp3r-web-tmp
+
+#To deploy in PRO
+# docker run -it --name h3lper-web-tmp -p 1080:80 -d h3lp3r-web-tmp
+
+#To remove it from PRO
+# docker stop h3lper-web-tmp
+# docker rm h3lper-web-tmp
+
+
